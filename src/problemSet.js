cf4a2516794fcdf0c437d7d3743fb90e394b5e03
Kyle Maune <kmaune71@gmail.com>
2014-07-18 18:35:40 -0700

    added letter count solution

--- src/problemSet.js
+++ src/problemSet.js
@@ -2,7 +2,7 @@
 // WDI Functions problem set
 // splashinn
 
-// lists to be sorted
+// merge(arr1, arr2)
 var list1 = [1, 3, 6, 11, 12, 14, 16, 18, 20];
 var list2 = [2, 4, 5, 7, 8, 9, 10, 13, 15, 17, 19];
 
@@ -14,4 +14,23 @@ function merge(arr1, arr2) {
   }
   return result.concat(arr1).concat(arr2);
 }
-console.log(merge(list1, list2)); // will return sorted array
+console.log(merge(list1, list2));
+
+
+// letterCount(word)
+var word = "Kyle and Tina";
+
+function letterCount(letters) {
+  var result = {}
+
+  for(var i = 0; i < letters.length; i++) {
+
+    if (result[letters[i]]) {
+      result[letters[i]] += 1;
+    } else {
+      result[letters[i]] = 1;
+    }
+  }
+  return result;
+}
+console.log(letterCount(word));
